ba3dd66f79d49f2ef9b879d1b5d07d05ed896d41
Jason Shi <Jasonycshi@outlook.com>
2024-09-18 16:18:13 +0800

    reproduce NDS2023

---
+++ NDSS2023/gpu_test.py
@@ -0,0 +1,13 @@
+'''
+Author: Yuchen Shi
+Date: 17-09-2024 23:05:18
+Last Editors: Jason Shi
+Contact Last Editors: Jasonycshi@outlook.com
+LastEditTime: 17-09-2024 23:06:03
+'''
+import torch
+print("是否可用：", torch.cuda.is_available())        # 查看GPU是否可用
+print("GPU数量：", torch.cuda.device_count())        # 查看GPU数量
+print("torch方法查看CUDA版本：", torch.version.cuda)  # torch方法查看CUDA版本
+print("GPU索引号：", torch.cuda.current_device())    # 查看GPU索引号
+print("GPU名称：", torch.cuda.get_device_name(0))    # 根据索引号得到GPU名称
